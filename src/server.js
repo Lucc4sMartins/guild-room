db9d91bd17ffc88f1167e6796951898e069c4e67
Luccas Martins <luccasmartins.nave@gmail.com>
2020-02-27 20:49:25 -0300

    Updating server port

--- src/server.js
+++ src/server.js
@@ -6,4 +6,6 @@ const routes = require('./config/routes')
 app.use(express.json())
 app.use(routes)
 
-app.listen(3000)
+const port = process.env.PORT || 3000
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
