a1d9c340bfb9967be3ca665fb683a57aa128a52b
Luccas Martins <luccasmartins.nave@gmail.com>
2020-02-27 22:50:23 -0300

    Updating db name to db user name

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PW,
-  database: 'guild-room',
+  database: process.env.DB_USER,
   define: {
     timestamps: true,
     underscored: true
