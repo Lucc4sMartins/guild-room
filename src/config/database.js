d636cd8c0615aa9f433d958213819a71e885e877
Luccas Martins <luccasmartins.nave@gmail.com>
2020-02-27 22:11:32 -0300

    Adding ssl and env variables for db config

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,16 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: '123',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PW,
   database: 'guild-room',
   define: {
     timestamps: true,
     underscored: true
+  },
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 }
