cf1f219d345a011baa296aca199d97763cadb106
Luccas Martins <luccasmartins.nave@gmail.com>
2020-02-27 23:03:11 -0300

    Adding DB_NAME env variable

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PW,
-  database: 'd4q96vhupqp3t0',
+  database: process.env.DB_NAME,
   define: {
     timestamps: true,
     underscored: true
