38689cba6400da898d81c75d59ea41eceb8396bd
Luccas Martins <luccasmartins.nave@gmail.com>
2020-02-27 22:52:13 -0300

    Updating db name

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PW,
-  database: process.env.DB_USER,
+  database: 'd4q96vhupqp3t0',
   define: {
     timestamps: true,
     underscored: true
